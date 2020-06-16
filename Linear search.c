5881f7069a3d6dd42e6ec5c6561fecb3d863e5f4
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-06-16 12:40:16 +0600

    Update Linear search.c

--- Linear search.c
+++ Linear search.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
-
-int search(int arr[], int n, int x)
+#include<stdio.h>
+int main()
 {
-	int i;
-	for (i = 0; i < n; i++)
-		if (arr[i] == x)
-			return i;
-	return -1;
-}
-
-int main(void)
-{
-	int arr[] = { 2, 3, 4, 10, 40 };
-
-	int x = 10;
-
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	int result = search(arr, n, x);
-
-	if(result == -1)
-        printf("Element is not present in array");
-
-    else
-        printf("Element is present at index %d",result);
-
-	return 0;
-}
+ 	int i,n,key,a[100];
+	
+ 	printf("Enter Array length : ");
+ 	scanf("%d",&n);
+	
+ 	for(i=0; i<n; i++)
+ 	{
+	 scanf("%d",&a[i]);
+ 	}
+	
+ 	printf("Enter Search key : ");
+	scanf("%d",&key);
+ 
+	for(i=0; i<n; i++)
+ 
+	{
+ 
+	if(a[i]==key)
+ 	{
+ 	printf("Found at : %d Location",i);
+ 	break;
+	}
+ 	}
+ 	if(i==n)
+ 	{
+ 	printf("Not Found!");
+ 	}
+ }
 
