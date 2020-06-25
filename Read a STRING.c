585f5ecf2b061ed8a627c73ee9d6a955eb98413c
Asadullah Al Imran <66588286+asadullah00alimran@users.noreply.github.com>
2020-06-25 23:04:42 +0600

    Add files via upload

---
+++ Read a STRING.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[50];
+
+    printf("Enter a String : ");
+    scanf("%s", str);
+
+    printf("\nThe given string is: %s\n", str);
+
+    return 0;
+}
+
